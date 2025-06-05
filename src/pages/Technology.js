ba3853f35d3216c1e022c75db62d4f302cd3d5bd
Emir Dogdibegovic <edogdibeg@alchemity.tech>
2025-06-05 07:30:11 -0700

    minor changes on technology and home page

--- src/pages/Technology.js
+++ src/pages/Technology.js
@@ -568,8 +568,8 @@ function Technology() {
                 ...bulletPointsStyle,
                 marginTop: '2rem'
               }}>
-                <div style={bulletPointStyle}>Non-oxidative single-step conversion of methane, CO₂, and water from waste feedstocks to clean chemicals/fuels without CO₂ emissions. Schematics shows a ceramic membrane reactor (strontium cerate) filled with low-cost iron-silica catalyst inside a reactor vessel.</div>
-                <div style={bulletPointStyle}>In this example methane flows through the core and sweep gas (air) circulates outside. Hydrogen is extracted in the catalyst bed and transported through the membrane to the sweep side via Le Chatelier's Principle, where it reacts with oxygen to form water and heat—enabling autothermal, energy-efficient operation.</div>
+                <div style={bulletPointStyle}>Non-oxidative single-step conversion of biomethane, CO₂, and water from waste feedstocks to clean chemicals/fuels without CO₂ emissions. Schematics shows a ceramic membrane reactor (strontium cerate) filled with low-cost iron-silica catalyst inside a reactor vessel.</div>
+                <div style={bulletPointStyle}>In this example biomethane flows through the core and sweep gas (air) circulates outside. Hydrogen is extracted in the catalyst bed and transported through the membrane to the sweep side via Le Chatelier's Principle, where it reacts with oxygen to form water and heat—enabling autothermal, energy-efficient operation.</div>
                 <div style={bulletPointStyle}>Oxygen ions from air sweep diffuse inward to react with carbon, forming trace CO, which prevents coking and extends catalyst life and reactor durability.</div>
                 <div style={bulletPointStyle}>Products include olefins and aromatics. System is tunable via temperature, pressure, recycles, and sweep gas to meet specific product demands.</div>
               </div>
